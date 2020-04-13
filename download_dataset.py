786789e225c17f5f5f7ff159a6906bd9437a8273
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-04-13 18:30:58 +0800

    Added script to download dataset

---
+++ download_dataset.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+import urllib.request
+
+if __name__ == '__main__':
+    print('INFO - Downloading dataset archive ...')
+    urllib.request.urlretrieve('https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz', 'aclImdb_v1.tar.gz')
+
+    print('INFO - Unpacking archive ...')
+    shutil.unpack_archive('aclImdb_v1.tar.gz', '.')
+
+    print('INFO - Cleaning up ...')
+    os.unlink('aclImdb_v1.tar.gz')
